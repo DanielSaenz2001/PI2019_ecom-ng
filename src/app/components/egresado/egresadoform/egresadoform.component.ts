3e23bf615071a801594da9de8dcd1fc74e1df8c4
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2019-11-19 16:21:52 -0500

    egresado

--- src/app/components/egresado/egresadoform/egresadoform.component.ts
+++ src/app/components/egresado/egresadoform/egresadoform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-egresadoform',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EgresadoformComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) { }
+  egresadoform: FormGroup;
   ngOnInit() {
+    this.egresadoform = this.formBuilder.group({
+      id:  [''],
+      codigo: ['', [Validators.required, Validators.minLength(8),Validators.pattern('[0-9]*')]],
+      celular: ['', [Validators.required, Validators.minLength(8),Validators.pattern('[0-9]*')]],
+      estado_actualizaciones: ['0'],
+      fec_actualizacion: [''],
+      email: ['', [Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      persona_id: [''],
+      domicilio_actual: [''],
+    });
   }
 
 }
