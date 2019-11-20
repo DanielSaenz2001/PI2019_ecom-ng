66027c7b5fd5bfd356ebb8aa1f6ba73826b2b4be
joseL32 <jose.orosco@upeu.edu.pe>
2019-11-20 18:49:31 -0500

    agregando empresasFrom

--- src/app/components/empresas/empresasform/empresasform.component.ts
+++ src/app/components/empresas/empresasform/empresasform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EmpresasService } from 'src/app/services/empresas.service';
 @Component({
   selector: 'app-empresasform',
   templateUrl: './empresasform.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresasformComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder, private empresasService:EmpresasService) { }
+  empresaFrom: FormGroup;
   ngOnInit() {
+    this.empresaFrom = this.formBuilder.group({
+      id:  [''],
+      nombre: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.minLength(9),Validators.pattern('[0-9]*')]],
+      tipo: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      correo: ['', [Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+    });
   }
-
+  save(){
+    this.empresaFrom.value.egresado=1;
+    console.log(this.empresaFrom.value)
+    this.empresasService.add(this.empresaFrom.value).subscribe();
+  }
 }
