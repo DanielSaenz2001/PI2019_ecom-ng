c51051c0428711c2cbfb85450a8024dc6d7e5346
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2019-11-20 19:10:21 -0500

    egresados

--- src/app/components/egresado/egresadoform/egresadoform.component.ts
+++ src/app/components/egresado/egresadoform/egresadoform.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { PaisesService } from 'src/app/services/paises.service';
 import { DepartamentosService } from 'src/app/services/departamentos.service';
 import { ProvinciasService } from 'src/app/services/provincias.service';
 import { EgresadosService } from 'src/app/services/egresados.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-egresadoform',
   templateUrl: './egresadoform.component.html',
-  styleUrls: ['./egresadoform.component.css']
+  styleUrls: ['./egresadoform.component.css'],
+  providers: [DatePipe]
 })
 export class EgresadoformComponent implements OnInit {
-
-  constructor(private formBuilder: FormBuilder,
+  @Input() ID:string;
+  constructor( private datePipe: DatePipe,
+    private formBuilder: FormBuilder,
     private paisesService:PaisesService,
     private departamentosService:DepartamentosService,
     private provinciasService:ProvinciasService,
+    private personaServices: PersonaService,
     private egresadosService:EgresadosService,
     private router: Router,
     private route: ActivatedRoute) { }
-  egresadoform: FormGroup;
-  paises;
+    egresadoform: FormGroup;
+    personaForm: FormGroup;
+    paises;
     departamentos;
     usuarios;
     provincias;
@@ -38,11 +44,11 @@ export class EgresadoformComponent implements OnInit {
       departamento: ['', [Validators.required]],
       provincia: ['', [Validators.required]],
     });
+    this.personaForm = this.formBuilder.group({});
   }
   pais(){
     this.paisesService.getlist().subscribe(response => {
       this.paises= response;
-      console.log(response)
     });
   }
   depar(){
@@ -56,17 +62,20 @@ export class EgresadoformComponent implements OnInit {
     });
   }
 
+  myDate;
   save(){
-    let id = this.route.snapshot.paramMap.get('id')
-   if(id != null){
-      this.egresadosService.update(id, this.egresadoform.value).subscribe(response => {
-        console.log(response)
-      });;
-      this.router.navigateByUrl('/profile');
-    }else{
-      console.log(this.egresadoform.value)
-      //this.egresadosService.add(this.egresadoform.value).subscribe(response => {
-      //});
-    }
+    this.myDate = new Date();
+    this.myDate = this.datePipe.transform(this.myDate, 'yyyy-MM-dd');
+    this.egresadoform.value.fec_actualizacion=this.myDate;
+    this.egresadoform.value.estado_actualizaciones='0';
+    this.egresadoform.value.persona_id=this.ID;
+    this.personaForm.value.activo=true
+    console.log(this.personaForm.value)
+    console.log(this.egresadoform.value)
+    this.egresadosService.add(this.egresadoform.value).subscribe();
+    this.personaServices.update(this.ID, this.personaForm.value).subscribe();
+  }
+  borrar(){
+    this.egresadoform.reset();
   }
 }
