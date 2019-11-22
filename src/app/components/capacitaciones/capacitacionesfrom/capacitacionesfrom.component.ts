ebdf0e5146e8eadf317c23b1ccebdd672f901133
joseL32 <jose.orosco@upeu.edu.pe>
2019-11-22 11:20:45 -0500

    complementando capacitaciones

--- src/app/components/capacitaciones/capacitacionesfrom/capacitacionesfrom.component.ts
+++ src/app/components/capacitaciones/capacitacionesfrom/capacitacionesfrom.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CapacitacionesService } from 'src/app/services/capacitaciones.service';
 @Component({
   selector: 'app-capacitacionesfrom',
   templateUrl: './capacitacionesfrom.component.html',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CapacitacionesfromComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private empresasService:CapacitacionesService) { }
+  capacitacionesFrom: FormGroup;
 
   ngOnInit() {
+    this.capacitacionesFrom = this.formBuilder.group({
+      id:  [''],
+      nombre: ['', [Validators.required]],
+      informacion: ['', [Validators.required]],
+      duracion: ['', [Validators.required]],
+      fecha_inicio: ['', [Validators.required]],
+      fecha_fin: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      precio: [''],
+      rutas: ['', [Validators.required]],
+      egresado_id: [''],
+      empresa_id: [''],
+    });
   }
-
+  save(){
+    this.capacitacionesFrom.value.egresado_id=1;
+    console.log(this.capacitacionesFrom.value)
+    //this.empresasService.add(this.capacitacionesFrom.value).subscribe();
+  }
 }
+
+
+
+
---
+++ src/app/services/capacitaciones.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CapacitacionesService {
+  
+  endPoint ='http://localhost:8000/api/capacitaciones'
+  constructor(private http:HttpClient) { }
+
+  public getlist(): Observable<any>{
+    return this.http.get<any>(`${this.endPoint}`)
+  }
+  public getById(id): Observable<any> {
+    return this.http.get<any>(`${this.endPoint}/${id}`);
+  }
+  public add(data): Observable<any> {
+    return this.http.post<any>(`${this.endPoint}`, data);
+  }
+  public update(id, data): Observable<any> {
+      return this.http.put<any>(`${this.endPoint}/${id}`, data);
+  }
+  public delete(id): Observable<any> {
+      
+      return this.http.delete<any>(`${this.endPoint}/${id}`);
+  }
+}
